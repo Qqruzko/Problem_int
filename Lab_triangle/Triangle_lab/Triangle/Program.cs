f9d3447c3985c0514fc5a105347402d99d87af7d
Qqruzko <150928737+Qqruzko@users.noreply.github.com>
2023-11-16 23:40:45 +0300

    Add files via upload

---
+++ Lab_triangle/Triangle_lab/Triangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter perimeter");
+            int perimeter =Console.Read();
+            int poluperimetr = perimeter / 2;
+            
+            Double side = (perimeter / 3);
+            
+            Double S = Math.Round(Math.Sqrt(poluperimetr * (poluperimetr - side) * (poluperimetr - side) * (poluperimetr - side)),2);
+            Console.WriteLine("Side = "+side);
+            Console.WriteLine("S = "+S);
+            Console.WriteLine("Done");
+        }
+    }
+}
